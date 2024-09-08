5998dbbb8c8f83d62a84887f7c27db4d626598a6
mirkoterzic <mirko.terzic2002@gmail.com>
2024-09-08 12:41:18 +0200

    added Category and Footer Component

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import HeadLineCards from "./components/HeadLineCards";
 import Food from "./components/Food";
+import Category from "./components/Category";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -11,6 +13,8 @@ function App() {
       <Hero />
       <HeadLineCards />
       <Food />
+      <Category />
+      <Footer />
     </>
   );
 }
