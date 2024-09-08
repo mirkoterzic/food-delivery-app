4ab938bb2762aa9fd44ce09f8b34aa861f83e415
mirkoterzic <mirko.terzic2002@gmail.com>
2024-09-08 12:00:38 +0200

    mapped and displayed the food

--- src/components/Food.jsx
+++ src/components/Food.jsx
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import { data } from "../data/data.js";
 
 const Food = () => {
+  const [foods, setFoods] = useState(data);
   return (
     <div className="max-w-[1640px] m-auto px-4 py-12">
       <h1 className="text-orange-500 font-bold text-4xl text-center">
@@ -49,7 +50,29 @@ const Food = () => {
           </div>
         </div>
       </div>
-      {/* Tommorow add displat foods with .map */}
+      {/* Display Foods */}
+      <div className="grid grid-cols-2 lg:grid-cols-4 gap-6 pt-4">
+        {foods.map((item, index) => (
+          <div
+            key={index}
+            className="border shadow-lg hover:scale-105 duration-300 rounded-lg"
+          >
+            <img
+              src={item.image}
+              alt={item.name}
+              className="w-full h-[200px] object-cover rounded-t-lg "
+            />
+            <div className="flex justify-between px-2 py-4 ">
+              <p className="font-bold">{item.name}</p>
+              <p>
+                <span className="bg-orange-500 text-white p-2 rounded-xl">
+                  {item.price}
+                </span>
+              </p>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
